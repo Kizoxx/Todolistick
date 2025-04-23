5a060bdaba750f320895007cfc0c8f3d1498db2c
Kizoxx <88961743+Kizoxx@users.noreply.github.com>
2025-04-23 18:03:15 +0000

    Update main.go

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
+	"time"
 )
 
 func main() {
@@ -13,7 +14,7 @@ func main() {
 	r := mux.NewRouter()
 
 	// Подключаемся к БД
-	dbStorage, err := storage.NewPostgresStorage("user=postgres password=kizoDB dbname=todolist sslmode=disable")
+	dbStorage, err := storage.NewPostgresStorage()
 	if err != nil {
 		log.Fatalf("Ошибка подключения к базе данных: %v", err)
 	}
@@ -29,12 +30,22 @@ func main() {
 	r.HandleFunc("/todos/{id:[0-9]+}", handler.Update).Methods("PUT")
 	r.HandleFunc("/todos/{id:[0-9]+}", handler.Delete).Methods("DELETE")
 
+	// Применяем middleware Logger ко всем маршрутам
+	loggedRouter := Logger(r)
+
 	// Запуск сервера
 	log.Println("Сервер запущен на порту 8080")
-
-	err = http.ListenAndServe(":8080", r)
+	err = http.ListenAndServe(":8080", loggedRouter)
 	if err != nil {
 		log.Fatalf("Ошибка запуска сервера: %v", err)
 	}
+}
 
+func Logger(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		log.Printf("[HTTP] %s %s from %s", r.Method, r.URL.Path, r.RemoteAddr)
+		next.ServeHTTP(w, r)
+		log.Printf("[HTTP] Completed in %v", time.Since(start))
+	})
 }
